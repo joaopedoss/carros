0cdacc8f19f17c4db4da0551bf1980d947119ea7
João Silveira <jpss06joao@gmail.com>
2024-03-26 21:22:27 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import Luxury from "./compenents/Luxury"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 lg:flex justify-center items-center antialiazed">
-      <section className="lg:flex max-w-[920px]">
+      <div className="lg:flex max-w-[920px]">
         <Sedans />
         <Suvs />
         <Luxury />
 
 
-      </section>
+      </div>
     </main>
   )
 }
